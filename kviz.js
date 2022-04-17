1403199487c185858aae7fa96b09f9800c33b30f
p-kost <petra.kostal1@gmail.com>
2022-04-17 14:56:56 +0200

    zatím pouze statické zobrazení

--- kviz.js
+++ kviz.js
@@ -1,40 +1,65 @@
-//data
+//data pro otazky
 
-let otazka1 = {
+let otazky = [
+    {
     otazka: 'Co je ikonická hračka z 80. let?',
-    moznostA: 'Kočičák',
-    moznostB: 'Mončičák',
-    moznostC: 'Opičák',
-    obrazek: 'obrazky/moncicak.jpg'
-};
+    odpovedA: 'Kočičák',
+    odpovedB: 'Mončičák',
+    odpovedC: 'Opičák',
+    poleMoznosti: ['Kočičák', 'Mončičák', 'Opičák'],
+    obrazek: 'obrazky/moncicak.jpg',
+    indexSpravne: 1,
+    cisloOtazky: 1
+    },
 
-let otazka2 = {
+    {
     otazka: 'Jaké je Matějovo nejoblíbenější ovoce?',
-    moznostA: 'Kokos',
-    moznostB: 'Melounek',
-    moznostC: 'Jahoda',
-    obrazek: 'obrazky/ovoce.jpg'
-};
+    odpovedA: 'Kokos',
+    odpovedB: 'Melounek',
+    odpovedC: 'Jahoda',
+    odpovedD: 'Ani jedna z možností',
+    poleMoznosti: ['Kokos','Melounek','Jahoda', 'Ani jedna z možností'],
+    obrazek: 'obrazky/ovoce.jpg',
+    indexSpravne: 1,
+    cisloOtazky: 2
+    },
 
-let otazka3 = {
+    {
     otazka: 'Pro úspěšné absolvování kurzu je potřeba..',
-    moznostA: 'Umět JavaScript',
-    moznostB: 'Chodit po kurzu do hospody',
-    obrazek: 'obrazky/pivo.jpg'
-};
+    odpovedA: 'Umět JavaScript',
+    odpovedB: 'Chodit po kurzu do hospody',
+    poleMoznosti: ['Umět JavaScript', 'Chodit po kurzu do hospody'],
+    obrazek: 'obrazky/pivo.jpg',
+    indexSpravne: 0,
+    cisloOtazky: 3
+    },
+];
+
+
+// vytahnuti promennych z html
+
+let kviz = document.querySelector('.kviz')
+let poradi = document.querySelector('#poradi')
+let otazka = document.querySelector('#otazka')
+let obsah = document.querySelector('.obsah')
+let foto = document.querySelector('.foto')
+let obrazek = document.querySelector('#obrazek')
+let moznosti = document.querySelector('#moznosti')
+let odpovedi = document.querySelector('#odpovedi');
+
+poradi.textContent = 'Otázka ' + otazky[0].cisloOtazky + '/3';
+obrazek.src = otazky[0].obrazek;
+otazka.textContent = otazky[0].otazka;
 
 
 
-//vlozeni otazky
+for (let i=0; i < otazky[0].poleMoznosti.length; i=i+1) {
 
-let otazka = document.querySelector('#otazka');
-otazka.textContent = otazka1.otazka;
+    let volba = document.createElement('li');
+    odpovedi.appendChild(volba);
 
-let odpovedA = document.querySelector('#a');
-odpovedA.textContent = otazka1.moznostA;
 
-let odpovedB = document.querySelector('#b');
-odpovedB.textContent = otazka1.moznostB;
+    volba.dataset.poleMoznosti = i;
+    volba.textContent = otazky[0].poleMoznosti[i];
+}
 
-let odpovedC = document.querySelector('#c');
-odpovedC.textContent = otazka1.moznostC;
